6fb47d5f1e2695a798d89a55362fce9bf4eb6bb9
valhugnat1 <hugo.philipp99@gmail.com>
2023-02-13 16:25:28 +0100

    newLongStep

--- src/multipli_io_file.py
+++ src/multipli_io_file.py
@@ -8,7 +8,7 @@ def entryStepFileIo (entryFile) :
         print (contents)
 
 
-    with open("file_output.txt", "w") as text_file:
+    with open("file_output.txt", "wb") as text_file:
         text_file.write("Result of step send in file output :) ")
     
     fileOjb = {"resultFile" : {"path": "file_output.txt"}}
---
+++ src/this_is_a_very_very_very_very_very_very_very_very_very_very_very_very_very_very_long_file_for_a_step.py
@@ -0,0 +1,9 @@
+import time
+
+def this_is_a_very_very_very_very_very_very_very_very_very_very_very_very_very_very_long_function_for_a_step () : 
+    pass
+
+for i in range (120) : 
+    print (i)
+
+    time.sleep(1)
