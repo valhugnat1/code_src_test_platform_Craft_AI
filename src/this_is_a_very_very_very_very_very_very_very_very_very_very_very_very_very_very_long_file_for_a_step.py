bfb9dadadc33aa050753ab0358182dbab219a2ec
valhugnat1 <hugo.philipp99@gmail.com>
2023-02-13 16:36:39 +0100

    shorter fct name

--- src/this_is_a_very_very_very_very_very_very_very_very_very_very_very_very_very_very_long_file_for_a_step.py
+++ src/this_is_a_very_very_very_very_very_very_very_very_very_very_very_very_very_very_long_file_for_a_step.py
@@ -1,6 +1,6 @@
 import time
 
-def this_is_a_very_very_very_very_very_very_very_very_very_very_very_very_very_very_long_function_for_a_step () : 
+def this_is_a_very_very_very_very_very_very_very_very_very_very_very_very_very_long_function_for_a_step () : 
     pass
 
 for i in range (120) : 
