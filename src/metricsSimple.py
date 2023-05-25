3becba37c126128a17c7118b07a6f857b7c237a0
valhugnat1 <hugo.philipp99@gmail.com>
2023-05-25 15:56:42 +0200

    step metrics

--- src/metricsSimple.py
+++ src/metricsSimple.py
@@ -1,9 +1,20 @@
 from craft_ai_sdk import *
+import time
+
+
 
 
 def metricsStep () :
 
     CraftAiSdk.record_metric_value("label", 12)
 
+    time.sleep(10)
+    CraftAiSdk.record_metric_value("label", 0.1409)
+    
+    time.sleep(10)
+    CraftAiSdk.record_metric_value("label", 1/3)
+
 
     print ("Metrics are send")
+
+
