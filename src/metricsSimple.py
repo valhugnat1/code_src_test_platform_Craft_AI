33fc2f4f3637c17a9175bf229124c9c3a0486cdf
valhugnat1 <hugo.philipp99@gmail.com>
2023-05-26 17:05:51 +0200

    Fix init SDK in metrics step

--- src/metricsSimple.py
+++ src/metricsSimple.py
@@ -1,18 +1,18 @@
-from craft_ai_sdk import *
+from craft_ai_sdk import CraftAiSdk
 import time
-
-
 
 
 def metricsStep () :
 
-    CraftAiSdk.record_metric_value("label", 12)
+    sdk = CraftAiSdk()
+
+    sdk.record_metric_value("label", 12)
 
     time.sleep(10)
-    CraftAiSdk.record_metric_value("label", 0.1409)
+    sdk.record_metric_value("label", 0.1409)
     
     time.sleep(10)
-    CraftAiSdk.record_metric_value("label", 1/3)
+    sdk.record_metric_value("label", 1/3)
 
 
     print ("Metrics are send")
