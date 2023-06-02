ff908c342090a3caed27c7c38082cb8caac233d6
valhugnat1 <hugo.philipp99@gmail.com>
2023-06-02 14:09:29 +0200

    update storage output file

--- src/multipli_io_file.py
+++ src/multipli_io_file.py
@@ -8,9 +8,14 @@ def entryStepFileIo (entryFile) :
         print (contents)
 
 
-    with open("file_output.txt", "wb") as text_file:
-        text_file.write("Result of step send in file output :) ")
+    # with open("file_output.txt", "wb") as text_file:
+    #     text_file.write("Result of step send in file output :) ")
+    #     # Assuming 'text_file' is the file object you're working with
+    text_file = open('file_output.txt', 'wb')  # Open the file in binary mode
+    text_file.write("Result of step send in file output :) ".encode('utf-8'))  # Encode the string to bytes
+    text_file.close()
+
     
     fileOjb = {"resultFile" : {"path": "file_output.txt"}}
 
-    return fileOjb 
+    return fileOjb
