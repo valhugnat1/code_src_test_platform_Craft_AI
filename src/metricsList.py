697333419296a86fc6ed1c0383d242826f9d0cfb
valhugnat1 <hugo.philipp99@gmail.com>
2023-06-19 14:45:26 +0200

    List metrics step

---
+++ src/metricsList.py
@@ -0,0 +1,24 @@
+from craft_ai_sdk import CraftAiSdk
+import time
+
+
+def metricsList () :
+
+    sdk = CraftAiSdk()
+
+    sdk.record_list_metric_values("metric", [12])
+
+
+    time.sleep(5)
+    sdk.record_metrecord_list_metric_valuesric_value("metric", 0.1409)
+    sdk.record_metrecord_list_metric_valuesric_value("metric", 0.19)
+    
+    time.sleep(5)
+    sdk.record_list_metric_values("loss", [12,2,9,8,4,9,4,2,0])
+
+    time.sleep(5)
+    sdk.record_list_metric_values("autre", [12,2,-9,8,-4,-9,-4,2,0])
+
+    print ("Metrics are send")
+
+
