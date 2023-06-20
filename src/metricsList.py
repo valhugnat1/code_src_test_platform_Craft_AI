4b7f2f078d3e26c9bcbcdfe0b70836e0878eb8d0
valhugnat1 <hugo.philipp99@gmail.com>
2023-06-20 14:39:25 +0200

    Big metrics list

--- src/metricsList.py
+++ src/metricsList.py
@@ -5,19 +5,24 @@ import time
 def metricsList () :
 
     sdk = CraftAiSdk()
+    sdk.record_metric_value("metric", 12)
+    sdk.record_metric_value("metric2", 100)
 
-    sdk.record_list_metric_values("metric", [12])
+    sdk.record_list_metric_values("metric", [0.172, 0,198, 0,112])
 
 
     time.sleep(5)
     sdk.record_list_metric_values("metric", 0.1409)
     sdk.record_list_metric_values("metric", 0.19)
     
-    time.sleep(5)
-    sdk.record_list_metric_values("loss", [12,2,9,8,4,9,4,2,0])
 
     time.sleep(5)
     sdk.record_list_metric_values("autre", [12,2,-9,8,-4,-9,-4,2,0])
+
+    for i in range (9000) : 
+
+        sdk.record_list_metric_values("1/x", 1/i)
+
 
     print ("Metrics are send")
 
