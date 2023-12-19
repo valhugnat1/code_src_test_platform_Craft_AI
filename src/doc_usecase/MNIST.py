2168cca58b2fa601eaeeb032d47f5f29b0c4d28d
Hugo Philipp <hugo.philipp99@gmail.com>
2023-12-19 17:06:51 +0100

    fashion_mnist

--- src/doc_usecase/MNIST.py
+++ src/doc_usecase/MNIST.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 from tensorflow.keras import layers, models
-from tensorflow.keras.datasets import mnist
+from tensorflow.keras.datasets import fashion_mnist
 from craft_ai_sdk import CraftAiSdk
 
 def trainer(learning_rate=0.001, nb_epoch=5, batch_size=64) : 
@@ -8,7 +8,7 @@ def trainer(learning_rate=0.001, nb_epoch=5, batch_size=64) :
     sdk = CraftAiSdk()
 
     # Load and preprocess the MNIST dataset
-    (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+    (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
     train_images = train_images.reshape((60000, 28, 28, 1)).astype('float32') / 255
     test_images = test_images.reshape((10000, 28, 28, 1)).astype('float32') / 255
     train_labels = tf.keras.utils.to_categorical(train_labels)
