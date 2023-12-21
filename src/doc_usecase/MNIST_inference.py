1490b7c306832f85d0a18f1ae3fe56622177f733
Hugo Philipp <hugo.philipp99@gmail.com>
2023-12-21 22:50:58 +0100

    path to image give in endpoint

--- src/doc_usecase/MNIST_inference.py
+++ src/doc_usecase/MNIST_inference.py
@@ -3,15 +3,17 @@ from tensorflow.keras.models import load_model
 import numpy as np
 from craft_ai_sdk import CraftAiSdk
 
-def inference(image_path, model_path, ground_truth):
+def inference(image, model_path, ground_truth):
 
     sdk = CraftAiSdk()
 
-    # Load the saved model
-    model = load_model(model_path)
+    # Save model in local context and load it
+    CraftAiSdk.download_data_store_object(model_path, "model.keras")
+    model = load_model("model.keras")
+
 
     # Preprocess the input image
-    input_image = tf.keras.preprocessing.image.load_img(image_path, target_size=(28, 28), color_mode='grayscale')
+    input_image = tf.keras.preprocessing.image.load_img(image["path"], target_size=(28, 28), color_mode='grayscale')
     input_image = tf.keras.preprocessing.image.img_to_array(input_image)
     input_image = np.expand_dims(input_image, axis=0)
     input_image = input_image.astype('float32') / 255.0
