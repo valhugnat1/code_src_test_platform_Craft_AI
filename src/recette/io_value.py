1b319324b9c0bc1a1fe803fdb6d9bc7231b1c063
Hugo Philipp <hugo.philipp99@gmail.com>
2024-02-06 15:38:48 +0100

    force int

--- src/recette/io_value.py
+++ src/recette/io_value.py
@@ -2,6 +2,6 @@ def io_value (num_i, text_i, json_i):
     
     json_i.update({"last": True})
 
-    text_o = str(text_i)*num_i
+    text_o = str(text_i)*int(num_i)
 
     return {"text_o": text_o, "json_o": json_i, "bool_o": True}
