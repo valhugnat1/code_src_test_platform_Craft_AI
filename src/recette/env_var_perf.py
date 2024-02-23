f9a17086bdb54624a08f386723400751235f7256
Hugo Philipp <hugo.philipp99@gmail.com>
2024-02-23 16:54:04 +0100

    env var not working

--- src/recette/env_var_perf.py
+++ src/recette/env_var_perf.py
@@ -7,9 +7,9 @@ def env_var_perf(value_i) :
 
     value = int(value_i)
 
-    print(sdk.list_environment_variables())
+    # print(sdk.list_environment_variables())
 
-    sdk.create_or_update_environment_variable("PROD_PERF_TESTING", value**2)
+    # sdk.create_or_update_environment_variable("PROD_PERF_TESTING", value**2)
 
     return {"value_o": value+10}
 
